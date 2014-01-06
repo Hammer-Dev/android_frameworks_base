79265bec56a6330ec5f5ba665b73a867fc129c5b
David96 <xd_e-mail@gmx.de>
2014-01-06 07:48:33 -0200

    Frameworks: Lockscreen Blur
      If user selects "See through" under screen security,
      the locksceen background will now appear transparent
      with the content behind it blurred.
    
    Modificaitons made to the orginal commit to support Hammerhead and Flo.
    It seems full HD images causes rendering artifacts. To prevent this,
    we scale the image size to something more manageable.
    
    PS2: Reduce calls to System.Settngs
         Move bitmap scaling to DisplayPowerController
    PS3: Fix styling
    PS4: Fixes: 1. Black wallpaper after turning off see through
                2. Wallpaper flicker on unlock when blur is enabled
    PS5: Ignore status bar and nav bar when capturing screenshots
    PS6: 1. Remove content observer and unnecessary calls from viewManager
         2. Restructure the flow of setting the custom image
             * now closely follows setting album art as lockscreen image
    
    Change-Id: I4d83c17639799c36d5e62e22a2e71f102024b6b2
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -16,7 +16,6 @@
 
 package com.android.keyguard;
 
-import android.app.PendingIntent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
@@ -24,7 +23,6 @@ import android.graphics.drawable.TransitionDrawable;
 import com.android.internal.policy.IKeyguardShowCallback;
 import com.android.internal.widget.LockPatternUtils;
 
-import android.app.Activity;
 import android.app.ActivityManager;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
@@ -48,6 +46,11 @@ import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.Vibrator;
 import android.provider.Settings;
+import android.renderscript.Allocation;
+import android.renderscript.Allocation.MipmapControl;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 import android.util.Log;
 import android.util.Slog;
 import android.util.SparseArray;
@@ -92,7 +95,12 @@ public class KeyguardViewManager {
     private boolean mScreenOn = false;
     private LockPatternUtils mLockPatternUtils;
 
+<<<<<<< HEAD
     private boolean mUnlockKeyDown = false;
+=======
+    // Lockscreen blur
+    private Bitmap mBlurredImage = null;
+>>>>>>> d503697... Frameworks: Lockscreen Blur
 
     private KeyguardUpdateMonitorCallback mBackgroundChanger = new KeyguardUpdateMonitorCallback() {
         @Override
@@ -120,6 +128,10 @@ public class KeyguardViewManager {
         mViewManager = viewManager;
         mViewMediatorCallback = callback;
         mLockPatternUtils = lockPatternUtils;
+<<<<<<< HEAD
+=======
+
+>>>>>>> d503697... Frameworks: Lockscreen Blur
     }
 
     /**
@@ -165,6 +177,34 @@ public class KeyguardViewManager {
     private boolean shouldEnableTranslucentDecor() {
         Resources res = mContext.getResources();
         return res.getBoolean(R.bool.config_enableLockScreenTranslucentDecor);
+    }
+
+    // Lockscreen blur
+    public void setBackgroundBitmap(Bitmap bmp) {
+        if (bmp != null) {
+            mBlurredImage = blurBitmap(bmp, bmp.getWidth() < 900 ? 14: 18);
+        } else {
+            mBlurredImage = null;
+        }
+    }
+
+    private Bitmap blurBitmap(Bitmap bmp, int radius) {
+        Bitmap out = Bitmap.createBitmap(bmp);
+        RenderScript rs = RenderScript.create(mContext);
+
+        Allocation input = Allocation.createFromBitmap(
+                rs, bmp, MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
+        Allocation output = Allocation.createTyped(rs, input.getType());
+
+        ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        script.setInput(input);
+        script.setRadius(radius);
+        script.forEach(output);
+
+        output.copyTo(out);
+
+        rs.destroy();
+        return out;
     }
 
     class ViewManagerHost extends FrameLayout {
@@ -275,7 +315,12 @@ public class KeyguardViewManager {
             if (bgAspect > vAspect) {
                 background.setBounds(0, 0, (int) (vHeight * bgAspect), vHeight);
             } else {
+<<<<<<< HEAD
                 background.setBounds(0, 0, vWidth, (int) (vWidth / bgAspect));
+=======
+                mCustomBackground.setBounds(0, 0, vWidth,
+                        (int) (vWidth * (vAspect >= 1 ? bgAspect : (1 / bgAspect))));
+>>>>>>> d503697... Frameworks: Lockscreen Blur
             }
         }
 
@@ -526,6 +571,10 @@ public class KeyguardViewManager {
         mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
 
         mKeyguardHost.restoreHierarchyState(mStateContainer);
+
+        if (mBlurredImage != null) {
+            KeyguardUpdateMonitor.getInstance(mContext).dispatchSetBackground(mBlurredImage);
+        }
     }
 
     private void inflateKeyguardView(Bundle options) {
@@ -599,7 +648,10 @@ public class KeyguardViewManager {
         } else {
             mWindowLayoutParams.flags &= ~WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
         }
+<<<<<<< HEAD
         mWindowLayoutParams.format = show ? PixelFormat.TRANSLUCENT : PixelFormat.OPAQUE;
+=======
+>>>>>>> d503697... Frameworks: Lockscreen Blur
 
         mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
     }
